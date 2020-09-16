b33c79025930c52b888b18b407ab9e8e6778bbb8
Lee Prout <wleeprout@gmail.com>
2020-09-16 12:14:36 -0400

    added lists to bring in data from csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,22 +3,80 @@ import csv
 
 budget_csv = os.path.join(".", "Resources", "budget_data.csv")
 
-# testing to see if file path to csv was correct
+months = []
+profit_losses = []
+profit_change = []
 with open(budget_csv, "r") as csv_file: 
      reader = csv.reader(csv_file, delimiter=",")
+     csv_header = next(reader)
+     #months = len(list(reader))
+     
+     
 
-     headers = next(reader)
-     print(headers)
+    
+     
+     for p_l in reader:
+         profit_losses.append(int(p_l[1]))
+         months.append(str(p_l[0]))
 
-def financial_analysis(budget_data):
+     for p_c in range(len(profit_losses)-1):
+         profit_change.append(profit_losses[p_c + 1] - profit_losses[p_c]) 
 
-    months = str(budget_data[0])
-    profits_losses = int(budget_data[1])
+     
+t_months = len(months)
+sum_profit_losses = sum(profit_losses)
+average_change = (sum(profit_change) / len(profit_change))
 
-    total_months = len(months)
-    print(total_months)
+print("Financial Analysis")
+print("---------------------------------------")
+print(f"Total Months: {t_months}")
+print (f"Total: {sum_profit_losses}")
+print (f"Average Change: {average_change}")
 
-    print(f"Total Months: {str(total_months)}")
+    #  net = []
+    #  for row in reader:
+    #     net = column [1]
+    #     total += value
+    #     print(f"Total: {net}")
+
+
+#for profits_losses in reader: 
+# with open(budget_csv, "r") as csv_file: 
+#     total = 0
+#     for profits_losses in reader:
+#         #with open(budget_csv, "r") as csv_file:
+#         next(reader)
+#         value = column [1]
+#         total += value
+#         print(f"Total: {profits_losses}")
+
+#     reader = csv.reader(csv_file, delimiter=",")
+#     next(reader)
+#     profits_losses = sum(list(reader))
+#     print(f"Total: {profits_losses}") 
+
+
+
+# for months in budget_csv: 
+
+
+#      months = str(budget_csv[0])
+#      profits_losses = int(budget_csv[1])
+
+     
+
+#      headers = next(reader)
+#      print(headers)
+
+# def financial_analysis(budget_data):
+
+#     months = str(budget_data[0])
+#     profits_losses = int(budget_data[1])
+
+#     total_months = len(months)
+#     print(total_months)
+
+#     print(f"Total Months: {str(total_months)}")
 
 
 

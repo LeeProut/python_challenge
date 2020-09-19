b40aa17a03589fce65935f446598e3822f31c8f4
Lee Prout <wleeprout@gmail.com>
2020-09-18 23:23:20 -0400

    getting candidate list and vote count dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,9 @@ import csv
 election_csv = os.path.join (".","Resources", "election_data.csv")
 
 voter_ID = []
-County = []
-Candidate = []
+Candidate_list = []
 
+Vote_count = {}
 
 with open(election_csv, "r") as csv_file: 
     reader = csv.reader(csv_file, delimiter=",")
@@ -14,11 +14,22 @@ with open(election_csv, "r") as csv_file:
 
     for row in reader:
         t_voters = (voter_ID.append(row[0]))  
+        
+        
+    candidate = row[2]    
+    for row in reader:
+        if candidate not in Candidate_list: 
+            Candidate_list.append(candidate)
+        if candidate not in Vote_count: 
+            Vote_count[candidate] = 1
+        else:
+            Vote_count[candidate] = Vote_count[candidate] + 1
 
+    for candidate in Candidate_list: 
+        print((candidate, Vote_count[candidate], Vote_count[candidate] / len(voter_ID)) * 100)  
 
-
-
+    
 print("Election Results")
 print("-----------------------------------------")
 print(len(voter_ID))
-print(f"Total Votes: {t_voters}")
+

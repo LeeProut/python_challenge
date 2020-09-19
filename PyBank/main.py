9d774ea1b31e0610b97cc8ba609dd02562a1bc4a
Lee Prout <wleeprout@gmail.com>
2020-09-19 18:13:07 -0400

    added code to print to text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,46 +20,6 @@ with open(budget_csv, "r") as csv_file:
      for p_c in range(len(profit_losses)-1):
          profit_change.append(profit_losses[p_c + 1] - profit_losses[p_c]) 
 
-    #  for months in range(len(months)):    
-    #      months[profit_change.index(max_profit_increase)+1]
-    #      months[profit_change.index(greatest_profit_decrease)+1]    
-    #      print(months[profit_change.index(max_profit_increase)+1])
-
-    #  for months in range(len(months)):
-    #      months[profit_change.index+1]
-    #      months    
-
-    #  for mm_yyyy in range(len(months)-1):
-    #      dates.append(int(months[mm_yyyy + 1]) - int(months[mm_yyyy])
-
-    #  for mm_yyyy in range(len(months)-1):
-    #      dates.append(months[mm_yyyy + 1] - months[mm_yyyy])
-
-    #  for mm_yyyy in range(len(months)-1):
-    #      dates.append(str(months[mm_yyyy + 1]) - str(months[mm_yyyy])
-
-    #  for mm_yyyy in range(len(months)-1):
-    #      dates.remove(months[mm_yyyy + 1] - months[mm_yyyy])        
-
-    #  for date_profit in dates:    
-    #      date_increase = str(max_profit_increase[date_profit])
-    #      date_decrease = str(greatest_profit_decrease[date_profit])    
-
-    #  for date_profit in dates:    
-    #      date_increase = str(max_profit_increase[date_profit])
-    #      date_decrease = str(greatest_profit_decrease[date_profit])  
-
-    #  for date_profit in range(len(dates)):
-    #      date_increase = str(max_profit_increase[date_profit])
-    #      date_decrease = str(greatest_profit_decrease[date_profit])  
-
-
-
-     #for date_profit in range(len(months)):   
-     #for inc_month in range(len(greatest_increase)-1):
-         #greatest_increase.append(profit)    
-
-         
      
 t_months = len(months)
 sum_profit_losses = sum(profit_losses)
@@ -75,8 +35,6 @@ greatest_profit_decrease = min(profit_change)
 month_max_profit_increase = months[profit_change.index(max_profit_increase)+1]
 month_greatest_profit_decrease = months[profit_change.index(greatest_profit_decrease)+1]
 
-#print(months[profit_change.index(max_profit_increase)+1])
-#print(months[profit_change.index(greatest_profit_decrease)+1])
 
 print("Financial Analysis")
 print("---------------------------------------")
@@ -85,60 +43,29 @@ print (f"Total: {sum_profit_losses}")
 print (f"Average Change: {average_change}")
 print (f"Greatest Increase in Profits: {max_profit_increase}")
 print (f"Greatest Decrease in Profits: {greatest_profit_decrease}")
-#print (f"Greatest Increase in Profits: {max_profit_increase} + {month_max_profit_increase}")
-#print (f"Greatest Decrease in Profits: {greatest_profit_decrease} + {month_greatest_profit_decrease}")
 print (f"Greatest Increase in Profits: {month_max_profit_increase} (${max_profit_increase})")
 print (f"Greatest Decrease in Profits: {month_greatest_profit_decrease} (${greatest_profit_decrease})")
 
-budget_csv = os.path.print("")
-#print (f"Greatest Increase in Profits: {date_increase}")
-#print (f"Greatest Decrease in Profits: {date_decrease}")
 
+#to print to a text file 
+text_output = (
+    "Financial Analysis\n"
+    "---------------------------------------\n"
+f"Total Months: {t_months}\n"
+f"Total: {sum_profit_losses}\n"
+f"Average Change: {average_change}\n"
+f"Greatest Increase in Profits: {max_profit_increase}\n"
+f"Greatest Decrease in Profits: {greatest_profit_decrease}\n"
+f"Greatest Increase in Profits: {month_max_profit_increase} (${max_profit_increase})\n"
+f"Greatest Decrease in Profits: {month_greatest_profit_decrease} (${greatest_profit_decrease})\n"
+)
 
-    #  net = []
-    #  for row in reader:
-    #     net = column [1]
-    #     total += value
-    #     print(f"Total: {net}")
+file = os.path.join(".", "Analysis", "PyBank_analysis.txt")
+#file = "Analysis/PyBank_analysis.txt"
+with open (file, "w") as text: 
+    text.write(text_output)
+    text.close     
 
-
-#for profits_losses in reader: 
-# with open(budget_csv, "r") as csv_file: 
-#     total = 0
-#     for profits_losses in reader:
-#         #with open(budget_csv, "r") as csv_file:
-#         next(reader)
-#         value = column [1]
-#         total += value
-#         print(f"Total: {profits_losses}")
-
-#     reader = csv.reader(csv_file, delimiter=",")
-#     next(reader)
-#     profits_losses = sum(list(reader))
-#     print(f"Total: {profits_losses}") 
-
-
-
-# for months in budget_csv: 
-
-
-#      months = str(budget_csv[0])
-#      profits_losses = int(budget_csv[1])
-
-     
-
-#      headers = next(reader)
-#      print(headers)
-
-# def financial_analysis(budget_data):
-
-#     months = str(budget_data[0])
-#     profits_losses = int(budget_data[1])
-
-#     total_months = len(months)
-#     print(total_months)
-
-#     print(f"Total Months: {str(total_months)}")
 
 
 
